1824d8132de2ba0a92a40a09bb8f855294a52aeb
Ahmad abdul malik <104676956+Malik22222@users.noreply.github.com>
2023-06-26 00:29:13 +0700

    Update __init__.py

--- Ubot/core/__init__.py
+++ Ubot/core/__init__.py
@@ -11,7 +11,7 @@ from .constants import *
 
 async def ajg(client):
     try:
-        await client.join_chat("executivebanget")
+        await client.join_chat("fairysupport")
         await client.join_chat("grouptelegram_id")
         await client.join_chat("nakama_asl")
     except BaseException:
